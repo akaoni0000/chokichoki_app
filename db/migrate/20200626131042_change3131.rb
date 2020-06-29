6be64366c4c6bd6950921b7db54af13a270de0d0
T.M <helloworld9171@gmail.com>
2020-06-29 20:19:25 +0900

    fixed migration files

--- db/migrate/20200626131042_change3131.rb
+++
@@ -1,9 +0,0 @@
-class Change3131 < ActiveRecord::Migration[5.2]
-  def up
-    drop_table :style_images
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20200626154112_change3ur1.rb
+++
@@ -1,6 +0,0 @@
-class Change3ur1 < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :style_images, :style_images, :string
-    add_column :style_images, :images, :string
-  end
-end
