610c986c5d8d5589d81f9039183cde0d8ae9e4ad
T.M <helloworld9171@gmail.com>
2020-07-04 16:42:24 +0900

    save

--- app/controllers/style_images_controller.rb
+++ app/controllers/style_images_controller.rb
@@ -8,14 +8,11 @@ class StyleImagesController < ApplicationController
 
     def update
         @style_image_add = StyleImage.new(style_images_params)
-        @style_image_add.save
         binding.pry
         @style_image = StyleImage.find_by(hairdresser_id: @current_hairdresser.id)
         @style_image.hair_images.push(@style_image_add.hair_images)
         @style_image.hair_images.flatten!
         @style_image.save
-        @style_image_add.destroy
-        #@style_image.update(style_images_params)
         redirect_to hairdresser_path(@current_hairdresser.id)
     end
 
