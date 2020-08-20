e928277e8f450b333e12fca7f98eb4c6a2f9fe80
T.M <helloworld9171@gmail.com>
2020-08-20 16:11:34 +0900

    整理

--- db/migrate/20200814020218_change234824141.rb
+++
@@ -1,6 +0,0 @@
-class Change234824141 < ActiveRecord::Migration[5.2]
-  def change
-      add_column :user_chats, :speaker, :string
-      add_column :hairdresser_chats, :speaker, :string
-  end
-end
