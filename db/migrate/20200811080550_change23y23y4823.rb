a33922bffd641b89c5f2849f08beaa5a78294093
T.M <helloworld9171@gmail.com>
2020-08-20 16:05:39 +0900

    デプロイ

--- db/migrate/20200811080550_change23y23y4823.rb
+++
@@ -1,6 +0,0 @@
-class Change23y23y4823 < ActiveRecord::Migration[5.2]
-  def change
-    add_column :hairdresser_chat_rooms, :room_id, :integer
-    add_column :user_chat_rooms, :room_id, :integer
-  end
-end
