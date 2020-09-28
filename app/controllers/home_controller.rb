cf3024d5b5894de12b67e27ea5e6dd62e067d174
T.M <helloworld9171@gmail.com>
2020-09-28 11:50:49 +0900

    homeコントローラ修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
     def user_top
-        #人気美容師 口コミが10件以上あり、評価平均が4.5以上の美容師のデータを取得
+        #人気美容師 口コミが5件以上あり、評価平均が4.5以上の美容師のデータを取得
         @hairdressers = Hairdresser.select {|hairdresser| hairdresser.hairdresser_comments.length >= 5 && hairdresser.reputation_point/hairdresser.hairdresser_comments.where.not(rate: 0.0).length >= 4.5}
 
-        #評判の美容師 口コミが10件以上あり、評価平均が4以上の美容師のデータをメニューのカテゴリーごとに取得
+        #評判の美容師 口コミが3件以上あり、評価平均が4以上の美容師のデータをメニューのカテゴリーごとに取得
         @hairdressers_for_reputation = Hairdresser.select {|hairdresser| hairdresser.hairdresser_comments.length >= 3 && hairdresser.reputation_point/hairdresser.hairdresser_comments.where.not(rate: 0.0).length >= 4}.sample(10)
         @menus = @hairdressers_for_reputation.map {|hairdresser| hairdresser.menus}
         @menus.flatten!
@@ -13,10 +13,10 @@ class HomeController < ApplicationController
         @curly_menus = @menus.select {|menu| menu.category[2,1] == "1"}
         @parma_menus = @menus.select {|menu| menu.category[3,1] == "1"}
 
-        @hairdresser_cut_id_arry = @cut_menus.map {|menu| menu.hairdresser_id}.uniq!
-        @hairdresser_color_id_arry = @color_menus.map {|menu| menu.hairdresser_id}.uniq!
-        @hairdresser_curly_id_arry = @curly_menus.map {|menu| menu.hairdresser_id}.uniq!
-        @hairdresser_parma_id_arry = @parma_menus.map {|menu| menu.hairdresser_id}.uniq!
+        @hairdresser_cut_id_arry = @cut_menus.map {|menu| menu.hairdresser_id}.uniq
+        @hairdresser_color_id_arry = @color_menus.map {|menu| menu.hairdresser_id}.uniq
+        @hairdresser_curly_id_arry = @curly_menus.map {|menu| menu.hairdresser_id}.uniq
+        @hairdresser_parma_id_arry = @parma_menus.map {|menu| menu.hairdresser_id}.uniq
 
         #gon
         gon.body = "white" #jsで背景を変える
