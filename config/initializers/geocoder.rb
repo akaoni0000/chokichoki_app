1a6b38127224d2b728ab8a4d16cc0cad64aceeed
T.M <helloworld9171@gmail.com>
2020-10-09 16:24:22 +0900

    環境変数修正

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: "AIzaSyAvHs9f3DrpaQXU20q672klP11qzHgn4WY"               # API key for geocoding service
+  api_key: "<%=ENV['GOOGLE_MAP_KEY']%>"               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
